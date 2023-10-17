8aded7a5db2127961a6697c507a007fa386dab5f
Juanocuato25 <juancarena2@hotmail.com>
2023-10-17 17:29:59 -0500

    second commit

---
+++ src/Entity/Interaction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InteractionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InteractionRepository::class)]
+class Interaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comment = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $user_favorite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function isUserFavorite(): ?bool
+    {
+        return $this->user_favorite;
+    }
+
+    public function setUserFavorite(bool $user_favorite): static
+    {
+        $this->user_favorite = $user_favorite;
+
+        return $this;
+    }
+}
